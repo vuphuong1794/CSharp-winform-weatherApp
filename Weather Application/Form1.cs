ec9de09081b02c5cf6a100b4597c150cb2fd0aa6
KhaiTapCode04 <2251120162@ut.edu.vn>
2024-07-17 15:16:53 +0700

    khai commit

--- Weather Application/Form1.cs
+++ Weather Application/Form1.cs
@@ -35,28 +35,22 @@ namespace WeatherApp
         {
             using (WebClient web = new WebClient())
             {
+                DateTime convertDateTime(long millisec)
+                {
+                   
+                }
                 string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", tbCity.Text, APIKey);
                 var json = web.DownloadString(url);
                 WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
                 pic_icon.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
                 lab_condtion.Text = Info.weather[0].main;
                 lab_detail.Text = Info.weather[0].description;
-                lab_sunset.Text = Info.sys.sunset.ToString();
-                lab_sunrise.Text = Info.sys.sunrise.ToString();
+               
                 lab_windspeed.Text = Info.wind.speed.ToString();
                 lab_pressure.Text = Info.main.pressure.ToString();
 
             }
         }
-
-        DateTime convertDateTime(long millisec)
-        {
-            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            day = day.AddSeconds(millisec).ToLocalTime();
-            return day;
-        }
-
-
         private void btn_close_Click(object sender, EventArgs e)
         {
             Application.Exit();
