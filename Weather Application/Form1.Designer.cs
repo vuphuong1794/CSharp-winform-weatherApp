c1a88edbf5537916e46bc9b6ccb044a33768694b
vuphuong1794 <phuongvu1794@gmail.com>
2024-07-17 09:29:14 +0700

    delete .vs

--- Weather Application/Form1.Designer.cs
+++ Weather Application/Form1.Designer.cs
@@ -272,6 +272,7 @@
             this.Controls.Add(this.btn_close);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
