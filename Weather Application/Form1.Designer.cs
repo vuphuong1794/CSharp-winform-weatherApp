cefdbc5135c0553943f4e37f0262e9a5cfd33158
vuphuong1794 <phuongvu1794@gmail.com>
2024-07-19 09:44:08 +0700

    add convert time

--- Weather Application/Form1.Designer.cs
+++ Weather Application/Form1.Designer.cs
@@ -55,29 +55,28 @@
             this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_close.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_close.Location = new System.Drawing.Point(698, 0);
-            this.btn_close.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_close.Location = new System.Drawing.Point(931, 0);
+            this.btn_close.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_close.Name = "btn_close";
-            this.btn_close.Size = new System.Drawing.Size(26, 28);
+            this.btn_close.Size = new System.Drawing.Size(35, 34);
             this.btn_close.TabIndex = 0;
             this.btn_close.Text = "X";
             this.btn_close.UseVisualStyleBackColor = false;
             // 
             // tbCity
             // 
-            this.tbCity.Location = new System.Drawing.Point(252, 96);
-            this.tbCity.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbCity.Location = new System.Drawing.Point(336, 118);
+            this.tbCity.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbCity.Name = "tbCity";
-            this.tbCity.Size = new System.Drawing.Size(225, 20);
+            this.tbCity.Size = new System.Drawing.Size(299, 22);
             this.tbCity.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
@@ -87,10 +86,9 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(180, 91);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(240, 112);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 24);
+            this.label2.Size = new System.Drawing.Size(61, 28);
             this.label2.TabIndex = 3;
             this.label2.Text = "City";
             // 
@@ -100,10 +98,9 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(251, 28);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(335, 34);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(239, 29);
+            this.label3.Size = new System.Drawing.Size(300, 38);
             this.label3.TabIndex = 4;
             this.label3.Text = "Weather Forecast";
             // 
@@ -113,10 +110,10 @@
             this.btn_search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_search.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_search.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_search.Location = new System.Drawing.Point(489, 84);
-            this.btn_search.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_search.Location = new System.Drawing.Point(652, 103);
+            this.btn_search.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_search.Name = "btn_search";
-            this.btn_search.Size = new System.Drawing.Size(76, 30);
+            this.btn_search.Size = new System.Drawing.Size(101, 37);
             this.btn_search.TabIndex = 5;
             this.btn_search.Text = "Search";
             this.btn_search.UseVisualStyleBackColor = false;
@@ -125,10 +122,10 @@
             // pic_icon
             // 
             this.pic_icon.BackColor = System.Drawing.Color.Transparent;
-            this.pic_icon.Location = new System.Drawing.Point(130, 163);
-            this.pic_icon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic_icon.Location = new System.Drawing.Point(173, 201);
+            this.pic_icon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pic_icon.Name = "pic_icon";
-            this.pic_icon.Size = new System.Drawing.Size(96, 103);
+            this.pic_icon.Size = new System.Drawing.Size(128, 127);
             this.pic_icon.TabIndex = 6;
             this.pic_icon.TabStop = false;
             // 
@@ -138,10 +135,9 @@
             this.lab_condtion.BackColor = System.Drawing.Color.Transparent;
             this.lab_condtion.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lab_condtion.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lab_condtion.Location = new System.Drawing.Point(253, 163);
-            this.lab_condtion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lab_condtion.Location = new System.Drawing.Point(337, 201);
             this.lab_condtion.Name = "lab_condtion";
-            this.lab_condtion.Size = new System.Drawing.Size(122, 24);
+            this.lab_condtion.Size = new System.Drawing.Size(143, 28);
             this.lab_condtion.TabIndex = 7;
             this.lab_condtion.Text = "Conditions";
             // 
@@ -151,10 +147,9 @@
             this.lab_detail.BackColor = System.Drawing.Color.Transparent;
             this.lab_detail.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lab_detail.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lab_detail.Location = new System.Drawing.Point(254, 211);
-            this.lab_detail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lab_detail.Location = new System.Drawing.Point(339, 260);
             this.lab_detail.Name = "lab_detail";
-            this.lab_detail.Size = new System.Drawing.Size(72, 24);
+            this.lab_detail.Size = new System.Drawing.Size(85, 28);
             this.lab_detail.TabIndex = 8;
             this.lab_detail.Text = "Detail";
             // 
@@ -164,10 +159,9 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label6.Location = new System.Drawing.Point(254, 265);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(339, 326);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(99, 24);
+            this.label6.Size = new System.Drawing.Size(118, 28);
             this.label6.TabIndex = 9;
             this.label6.Text = "Sunrise:";
             // 
@@ -177,10 +171,9 @@
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label7.Location = new System.Drawing.Point(254, 313);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Location = new System.Drawing.Point(339, 385);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(99, 24);
+            this.label7.Size = new System.Drawing.Size(119, 28);
             this.label7.TabIndex = 10;
             this.label7.Text = "Sunset: ";
             // 
@@ -190,10 +183,9 @@
             this.lab_sunrise.BackColor = System.Drawing.Color.Transparent;
             this.lab_sunrise.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lab_sunrise.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lab_sunrise.Location = new System.Drawing.Point(338, 265);
-            this.lab_sunrise.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lab_sunrise.Location = new System.Drawing.Point(451, 326);
             this.lab_sunrise.Name = "lab_sunrise";
-            this.lab_sunrise.Size = new System.Drawing.Size(49, 24);
+            this.lab_sunrise.Size = new System.Drawing.Size(58, 28);
             this.lab_sunrise.TabIndex = 11;
             this.lab_sunrise.Text = "N/A";
             // 
@@ -203,10 +195,9 @@
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label9.Location = new System.Drawing.Point(409, 163);
-            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Location = new System.Drawing.Point(545, 201);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(145, 24);
+            this.label9.Size = new System.Drawing.Size(171, 28);
             this.label9.TabIndex = 12;
             this.label9.Text = "Wind Speed:";
             // 
@@ -216,10 +207,9 @@
             this.lab_windspeed.BackColor = System.Drawing.Color.Transparent;
             this.lab_windspeed.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lab_windspeed.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lab_windspeed.Location = new System.Drawing.Point(540, 163);
-            this.lab_windspeed.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lab_windspeed.Location = new System.Drawing.Point(720, 201);
             this.lab_windspeed.Name = "lab_windspeed";
-            this.lab_windspeed.Size = new System.Drawing.Size(49, 24);
+            this.lab_windspeed.Size = new System.Drawing.Size(58, 28);
             this.lab_windspeed.TabIndex = 13;
             this.lab_windspeed.Text = "N/A";
             // 
@@ -229,10 +219,9 @@
             this.label11.BackColor = System.Drawing.Color.Transparent;
             this.label11.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label11.Location = new System.Drawing.Point(409, 211);
-            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Location = new System.Drawing.Point(545, 260);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(109, 24);
+            this.label11.Size = new System.Drawing.Size(132, 28);
             this.label11.TabIndex = 14;
             this.label11.Text = "Pressure:";
             // 
@@ -242,10 +231,9 @@
             this.lab_pressure.BackColor = System.Drawing.Color.Transparent;
             this.lab_pressure.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lab_pressure.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lab_pressure.Location = new System.Drawing.Point(540, 211);
-            this.lab_pressure.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lab_pressure.Location = new System.Drawing.Point(720, 260);
             this.lab_pressure.Name = "lab_pressure";
-            this.lab_pressure.Size = new System.Drawing.Size(49, 24);
+            this.lab_pressure.Size = new System.Drawing.Size(58, 28);
             this.lab_pressure.TabIndex = 15;
             this.lab_pressure.Text = "N/A";
             this.lab_pressure.Click += new System.EventHandler(this.label12_Click);
@@ -256,20 +244,19 @@
             this.lab_sunset.BackColor = System.Drawing.Color.Transparent;
             this.lab_sunset.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lab_sunset.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.lab_sunset.Location = new System.Drawing.Point(338, 313);
-            this.lab_sunset.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lab_sunset.Location = new System.Drawing.Point(451, 385);
             this.lab_sunset.Name = "lab_sunset";
-            this.lab_sunset.Size = new System.Drawing.Size(49, 24);
+            this.lab_sunset.Size = new System.Drawing.Size(58, 28);
             this.lab_sunset.TabIndex = 16;
             this.lab_sunset.Text = "N/A";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(724, 441);
+            this.ClientSize = new System.Drawing.Size(965, 543);
             this.Controls.Add(this.lab_sunset);
             this.Controls.Add(this.lab_pressure);
             this.Controls.Add(this.label11);
@@ -287,9 +274,10 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbCity);
             this.Controls.Add(this.btn_close);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.pic_icon)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
